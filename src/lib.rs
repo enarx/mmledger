38ef3d535672dcdf709a67e6d32119cd0b643af2
Jarkko Sakkinen <jarkko@profian.com>
2022-03-24 05:50:00 +0200

    refactor: replace access with a generic type
    Closes: #8
    Signed-off-by: Jarkko Sakkinen <jarkko@profian.com>

--- src/lib.rs
+++ src/lib.rs
@@ -10,47 +10,31 @@ use core::cmp::Ordering;
 use lset::{Empty, Line, Span};
 use primordial::{Address, Offset, Page};
 
-bitflags::bitflags! {
-    /// Memory access permissions.
-    #[derive(Default)]
-    #[repr(transparent)]
-    pub struct Access: usize {
-        /// Read access
-        const READ = 1 << 0;
-
-        /// Write access
-        const WRITE = 1 << 0;
-
-        /// Execute access
-        const EXECUTE = 1 << 0;
-    }
-}
-
 /// A ledger record.
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
-pub struct Record {
-    /// The covered region of memory.
+pub struct Record<V> {
+    /// A region of memory.
     pub region: Line<Address<usize, Page>>,
 
-    /// The access permissions.
-    pub access: Access,
+    /// The fill value.
+    pub value: Option<V>,
 
     length: usize,
 }
 
-impl Record {
-    const EMPTY: Record = Record {
-        region: Line::new(Address::NULL, Address::NULL),
-        access: Access::empty(),
-        length: 0,
-    };
-
-    fn new(region: Line<Address<usize, Page>>, access: Access) -> Self {
-        Record {
+impl<V: Eq + Copy> Record<V> {
+    #[inline]
+    fn new(region: Line<Address<usize, Page>>, value: Option<V>) -> Self {
+        Self {
             region,
-            access,
+            value,
             length: 0,
         }
+    }
+
+    #[inline]
+    fn empty() -> Self {
+        Self::new(Line::new(Address::NULL, Address::NULL), None)
     }
 }
 
@@ -80,11 +64,11 @@ pub enum Error {
 // fine-grained controls over allocation. For example, to allocate a 4k page,
 // the user can instantiate as `Ledger::<128>::new(..)`.
 #[derive(Clone, Debug)]
-pub struct Ledger<const N: usize> {
-    records: [Record; N],
+pub struct Ledger<V, const N: usize> {
+    records: [Record<V>; N],
 }
 
-impl<const N: usize> Ledger<N> {
+impl<V: Eq + Copy, const N: usize> Ledger<V, N> {
     /// Sort the records.
     fn sort(&mut self) {
         self.records_mut().sort_unstable_by(|l, r| {
@@ -101,14 +85,14 @@ impl<const N: usize> Ledger<N> {
     }
 
     /// Create a new instance.
-    pub const fn new(region: Line<Address<usize, Page>>) -> Self {
-        let mut records = [Record::EMPTY; N];
+    pub fn new(region: Line<Address<usize, Page>>) -> Self {
+        let mut records = [Record::empty(); N];
         records[0].region = region;
         Self { records }
     }
 
     /// Get an immutable view of the records.
-    pub fn records(&self) -> &[Record] {
+    pub fn records(&self) -> &[Record<V>] {
         let used = self.records[0].length;
         &self.records[1..][..used]
     }
@@ -116,20 +100,13 @@ impl<const N: usize> Ledger<N> {
     /// Get a mutable view of the records.
     ///
     /// This function MUST NOT be public.
-    fn records_mut(&mut self) -> &mut [Record] {
+    fn records_mut(&mut self) -> &mut [Record<V>] {
         let used = self.records[0].length;
         &mut self.records[1..][..used]
     }
 
     /// Insert a new record into the ledger.
-    pub fn insert(
-        &mut self,
-        region: impl Into<Line<Address<usize, Page>>>,
-        access: impl Into<Option<Access>>,
-        commit: bool,
-    ) -> Result<(), Error> {
-        // Make sure the record is valid.
-        let record = Record::new(region.into(), access.into().unwrap_or_default());
+    pub fn insert(&mut self, record: Record<V>) -> Result<(), Error> {
         if record.region.start >= record.region.end {
             return Err(Error::InvalidRegion);
         }
@@ -154,21 +131,15 @@ impl<const N: usize> Ledger<N> {
             }
 
             // Potentially merge with the `prev` slot.
-            if prev.access == record.access && prev.region.end == record.region.start {
-                if commit {
-                    prev.region.end = record.region.end;
-                }
-
+            if prev.value == record.value && prev.region.end == record.region.start {
+                prev.region.end = record.region.end;
                 return Ok(());
             }
 
             // Potentially merge with the `prev` slot
             if let Some(next) = iter.peek_mut() {
-                if next.access == record.access && next.region.start == record.region.end {
-                    if commit {
-                        next.region.start = record.region.start;
-                    }
-
+                if next.value == record.value && next.region.start == record.region.end {
+                    next.region.start = record.region.start;
                     return Ok(());
                 }
             }
@@ -193,15 +164,9 @@ impl<const N: usize> Ledger<N> {
     ) -> Result<Line<Address<usize, Page>>, Error> {
         let region = self.records[0].region;
 
-        let start = Record {
-            region: Line::new(region.start, region.start),
-            ..Default::default()
-        };
+        let start = Record::<V>::new(Line::new(region.start, region.start), None);
 
-        let end = Record {
-            region: Line::new(region.end, region.end),
-            ..Default::default()
-        };
+        let end = Record::<V>::new(Line::new(region.end, region.end), None);
 
         // Synthesize a starting window.
         let first = [start, *self.records().first().unwrap_or(&end)];
@@ -241,48 +206,48 @@ mod tests {
 
     use core::mem::{align_of, size_of};
 
-    const PREV: Record = Record {
+    const PREV: Record<()> = Record::<()> {
         region: Line {
             start: Address::new(0x4000usize),
             end: Address::new(0x5000usize),
         },
-        access: Access::empty(),
+        value: None,
         length: 0,
     };
 
-    const NEXT: Record = Record {
+    const NEXT: Record<()> = Record::<()> {
         region: Line {
             start: Address::new(0x8000),
             end: Address::new(0x9000),
         },
-        access: Access::empty(),
+        value: None,
         length: 0,
     };
 
-    const INDX: Record = Record {
+    const INDX: Record<()> = Record::<()> {
         region: Line {
             start: Address::new(0x1000),
             end: Address::new(0x10000),
         },
-        access: Access::empty(),
+        value: None,
         length: 2,
     };
 
-    const LEDGER: Ledger<3> = Ledger {
+    const LEDGER: Ledger<(), 3> = Ledger {
         records: [INDX, PREV, NEXT],
     };
 
     #[test]
     fn record_size_align() {
-        assert_eq!(size_of::<Record>(), size_of::<usize>() * 4);
-        assert_eq!(align_of::<Record>(), align_of::<usize>());
+        assert_eq!(size_of::<Record::<()>>(), size_of::<usize>() * 4);
+        assert_eq!(align_of::<Record::<()>>(), align_of::<usize>());
     }
 
     #[test]
     fn insert() {
         let start = Address::from(0x1000usize).lower();
         let end = Address::from(0x10000usize).lower();
-        let mut ledger = Ledger::<8>::new(Line::new(start, end));
+        let mut ledger = Ledger::<(), 8>::new(Line::new(start, end));
 
         let region = Line {
             start: Address::from(0xe000usize).lower(),
@@ -290,15 +255,15 @@ mod tests {
         };
 
         assert_eq!(ledger.records(), &[]);
-        ledger.insert(region, None, true).unwrap();
-        assert_eq!(ledger.records(), &[Record::new(region, Access::empty())]);
+        ledger.insert(Record::new(region, None)).unwrap();
+        assert_eq!(ledger.records(), &[Record::<()>::new(region, None)]);
     }
 
     #[test]
     fn find_free_front() {
         let start = Address::from(0x1000).lower();
         let end = Address::from(0x10000).lower();
-        let mut ledger = Ledger::<8>::new(Line::new(start, end));
+        let mut ledger = Ledger::<(), 8>::new(Line::new(start, end));
 
         let region = ledger.find_free(Offset::from_items(2), true).unwrap();
         let answer = Line {
@@ -307,7 +272,7 @@ mod tests {
         };
         assert_eq!(region, answer);
 
-        ledger.insert(answer, None, true).unwrap();
+        ledger.insert(Record::new(answer, None)).unwrap();
 
         let region = ledger.find_free(Offset::from_items(2), true).unwrap();
         let answer = Line {
@@ -321,7 +286,7 @@ mod tests {
     fn find_free_back() {
         let start = Address::from(0x1000).lower();
         let end = Address::from(0x10000).lower();
-        let mut ledger = Ledger::<8>::new(Line::new(start, end));
+        let mut ledger = Ledger::<(), 8>::new(Line::new(start, end));
 
         let region = ledger.find_free(Offset::from_items(2), false).unwrap();
         let answer = Line {
@@ -330,7 +295,7 @@ mod tests {
         };
         assert_eq!(region, answer);
 
-        ledger.insert(answer, None, true).unwrap();
+        ledger.insert(Record::new(answer, None)).unwrap();
 
         let region = ledger.find_free(Offset::from_items(2), false).unwrap();
         let answer = Line {
@@ -347,17 +312,17 @@ mod tests {
             end: Address::new(0x6000),
         };
 
-        const MERGED: Record = Record {
+        const MERGED: Record<()> = Record::<()> {
             region: Line {
                 start: Address::new(0x4000),
                 end: Address::new(0x6000),
             },
-            access: Access::empty(),
+            value: None,
             length: 0,
         };
 
         let mut ledger = LEDGER.clone();
-        ledger.insert(REGION, Access::empty(), true).unwrap();
+        ledger.insert(Record::new(REGION, None)).unwrap();
 
         assert_eq!(ledger.records[0].length, 2);
         assert_eq!(ledger.records[1], MERGED);
@@ -371,215 +336,20 @@ mod tests {
             end: Address::new(0x8000),
         };
 
-        const MERGED: Record = Record {
+        const MERGED: Record<()> = Record::<()> {
             region: Line {
                 start: Address::new(0x7000),
                 end: Address::new(0x9000),
             },
-            access: Access::empty(),
+            value: None,
             length: 0,
         };
 
         let mut ledger = LEDGER.clone();
-        ledger.insert(REGION, Access::empty(), true).unwrap();
+        ledger.insert(Record::new(REGION, None)).unwrap();
 
         assert_eq!(ledger.records[0].length, 2);
         assert_eq!(ledger.records[1], PREV);
         assert_eq!(ledger.records[2], MERGED);
     }
-
-    /*
-    use std::mem::{align_of, size_of};
-
-    use super::*;
-
-    const MEMORY_MAP_ADDRESS: Address =
-        unsafe { Address::new_unchecked((2 * Page::SIZE) as *mut c_void) };
-    const MEMORY_MAP_SIZE: usize = 3 * Page::SIZE;
-
-    #[test]
-    fn addr_region_equal() {
-        const A: Address = unsafe { Address::new_unchecked(Page::SIZE as *mut c_void) };
-        const B: Address = unsafe { Address::new_unchecked(Page::SIZE as *mut c_void) };
-
-        assert_eq!(Region::new(A, Page::SIZE), Region::new(B, Page::SIZE),);
-    }
-
-    #[test]
-    fn addr_region_not_equal() {
-        const A: Address = unsafe { Address::new_unchecked(Page::SIZE as *mut c_void) };
-        const B: Address = unsafe { Address::new_unchecked(MEMORY_MAP_SIZE as *mut c_void) };
-
-        assert!(Region::new(A, Page::SIZE) != Region::new(B, Page::SIZE));
-    }
-
-    #[test]
-    fn alloc_region_success() {
-        const A: Address = unsafe { Address::new_unchecked((2 * Page::SIZE) as *mut c_void) };
-        const B: Address = unsafe { Address::new_unchecked((4 * Page::SIZE) as *mut c_void) };
-
-        let mut m: AddressSpace<2> = AddressSpace::new(MEMORY_MAP_ADDRESS, MEMORY_MAP_SIZE);
-        let region_a = Region::new(A, Page::SIZE);
-        let region_b = Region::new(B, Page::SIZE);
-
-        m.insert_region(region_a, AddressSpaceFlags::empty())
-            .unwrap();
-        m.insert_region(region_b, AddressSpaceFlags::empty())
-            .unwrap();
-
-        let result = match m.allocate_region(Page::SIZE, AddressSpaceFlags::DRY_RUN) {
-            Ok(r) => r,
-            _ => panic!(),
-        };
-
-        assert_eq!(result, Region::new(MEMORY_MAP_ADDRESS, 3 * Page::SIZE));
-    }
-
-    #[test]
-    fn alloc_region_failure() {
-        const A: Address = unsafe { Address::new_unchecked((2 * Page::SIZE) as *mut c_void) };
-        const B: Address = unsafe { Address::new_unchecked((4 * Page::SIZE) as *mut c_void) };
-        const C: Address = unsafe { Address::new_unchecked((3 * Page::SIZE) as *mut c_void) };
-
-        let mut m: AddressSpace<2> = AddressSpace::new(MEMORY_MAP_ADDRESS, MEMORY_MAP_SIZE);
-        let region_a = Region::new(A, Page::SIZE);
-        let region_b = Region::new(B, Page::SIZE);
-        let region_c = Region::new(C, Page::SIZE);
-
-        m.insert_region(region_a, AddressSpaceFlags::empty())
-            .unwrap();
-        m.insert_region(region_b, AddressSpaceFlags::empty())
-            .unwrap();
-        m.insert_region(region_c, AddressSpaceFlags::empty())
-            .unwrap();
-
-        match m.allocate_region(Page::SIZE, AddressSpaceFlags::DRY_RUN) {
-            Err(AddressSpaceError::OutOfSpace) => (),
-            _ => panic!(),
-        }
-    }
-
-    #[test]
-    fn extend_region() {
-        const A: Address = unsafe { Address::new_unchecked((2 * Page::SIZE) as *mut c_void) };
-        const B: Address = unsafe { Address::new_unchecked((4 * Page::SIZE) as *mut c_void) };
-        const E: Address = unsafe { Address::new_unchecked((3 * Page::SIZE) as *mut c_void) };
-
-        let mut m: AddressSpace<1> = AddressSpace::new(MEMORY_MAP_ADDRESS, MEMORY_MAP_SIZE);
-        let region_a = Region::new(A, Page::SIZE);
-        let expected = Region::new(E, Page::SIZE);
-
-        println!("{:?}", region_a);
-
-        m.insert_region(region_a, AddressSpaceFlags::empty())
-            .unwrap();
-        let result = match m.extend_region(B) {
-            Ok(region) => region,
-            _ => panic!(),
-        };
-
-        assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn insert_region() {
-        const A: Address = unsafe { Address::new_unchecked((2 * Page::SIZE) as *mut c_void) };
-
-        let mut m: AddressSpace<1> = AddressSpace::new(MEMORY_MAP_ADDRESS, MEMORY_MAP_SIZE);
-        let region = Region::new(A, Page::SIZE);
-
-        let region = match m.insert_region(region, AddressSpaceFlags::empty()) {
-            Ok(region) => region,
-            _ => panic!(),
-        };
-
-        assert_eq!(region, Region::new(A, Page::SIZE));
-    }
-
-    #[test]
-    fn insert_adjacent() {
-        const A: Address = unsafe { Address::new_unchecked((2 * Page::SIZE) as *mut c_void) };
-        const B: Address = unsafe { Address::new_unchecked((3 * Page::SIZE) as *mut c_void) };
-
-        let mut m: AddressSpace<2> = AddressSpace::new(MEMORY_MAP_ADDRESS, MEMORY_MAP_SIZE);
-        let region_a = Region::new(A, Page::SIZE);
-        let region_b = Region::new(B, Page::SIZE);
-
-        m.insert_region(region_a, AddressSpaceFlags::empty())
-            .unwrap();
-
-        let region = match m.insert_region(region_b, AddressSpaceFlags::DRY_RUN) {
-            Ok(region) => region,
-            _ => panic!(),
-        };
-
-        assert_eq!(region, Region::new(A, 2 * Page::SIZE));
-    }
-
-    #[test]
-    fn insert_after_memory_map() {
-        const A: Address = unsafe { Address::new_unchecked((5 * Page::SIZE) as *mut c_void) };
-
-        let mut m: AddressSpace<2> = AddressSpace::new(MEMORY_MAP_ADDRESS, MEMORY_MAP_SIZE);
-        let region_a = Region::new(A, Page::SIZE);
-
-        match m.insert_region(region_a, AddressSpaceFlags::DRY_RUN) {
-            Err(AddressSpaceError::Overflow) => (),
-            _ => panic!(),
-        }
-    }
-
-    #[test]
-    fn insert_intersects() {
-        const A: Address = unsafe { Address::new_unchecked((2 * Page::SIZE) as *mut c_void) };
-        const B: Address = unsafe { Address::new_unchecked((2 * Page::SIZE) as *mut c_void) };
-
-        let mut m: AddressSpace<2> = AddressSpace::new(MEMORY_MAP_ADDRESS, MEMORY_MAP_SIZE);
-        let region_a = Region::new(A, Page::SIZE);
-        let region_b = Region::new(B, Page::SIZE);
-
-        m.insert_region(region_a, AddressSpaceFlags::empty())
-            .unwrap();
-        match m.insert_region(region_b, AddressSpaceFlags::DRY_RUN) {
-            Err(AddressSpaceError::Overlap) => (),
-            _ => panic!(),
-        }
-    }
-
-    #[test]
-    fn insert_not_intersects() {
-        const A: Address = unsafe { Address::new_unchecked((2 * Page::SIZE) as *mut c_void) };
-        const B: Address = unsafe { Address::new_unchecked((4 * Page::SIZE) as *mut c_void) };
-
-        let mut m: AddressSpace<2> = AddressSpace::new(MEMORY_MAP_ADDRESS, MEMORY_MAP_SIZE);
-        let region_a = Region::new(A, Page::SIZE);
-        let region_b = Region::new(B, Page::SIZE);
-
-        m.insert_region(region_a, AddressSpaceFlags::empty())
-            .unwrap();
-        let region_c = match m.insert_region(region_b, AddressSpaceFlags::DRY_RUN) {
-            Ok(region) => region,
-            _ => panic!(),
-        };
-
-        assert_eq!(region_c, region_b);
-    }
-
-    #[test]
-    fn insert_overflow() {
-        const A: Address = unsafe { Address::new_unchecked((2 * Page::SIZE) as *mut c_void) };
-        const B: Address = unsafe { Address::new_unchecked((4 * Page::SIZE) as *mut c_void) };
-
-        let mut m: AddressSpace<1> = AddressSpace::new(MEMORY_MAP_ADDRESS, MEMORY_MAP_SIZE);
-        let region_a = Region::new(A, Page::SIZE);
-        let region_b = Region::new(B, Page::SIZE);
-
-        m.insert_region(region_a, AddressSpaceFlags::empty())
-            .unwrap();
-        match m.insert_region(region_b, AddressSpaceFlags::DRY_RUN) {
-            Err(AddressSpaceError::OutOfCapacity) => (),
-            _ => panic!(),
-        }
-    }
-    */
 }
