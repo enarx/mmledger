acd09b55c0ae0869c74635f37567cdbd29b2c4d5
Jarkko Sakkinen <jarkko@profian.com>
2022-03-24 17:26:40 +0200

    feat: `subtract()`
    Closes: #4
    Signed-off-by: Jarkko Sakkinen <jarkko@profian.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 
 use core::cmp::Ordering;
 
-use lset::{Empty, Line, Span};
+use lset::{Contains, Empty, Line, Span};
 use primordial::{Address, Offset, Page};
 
 /// A range of tagged address space in a ledger.
@@ -43,13 +43,10 @@ impl Region {
 #[derive(Debug)]
 pub enum Error {
     /// Out of storage capacity
-    OutOfCapacity,
+    NoCapacity,
 
     /// No space for the region
     NoSpace,
-
-    /// Overlapping with the existing regions
-    Overlap,
 
     /// Invalid region given as input
     InvalidRegion,
@@ -69,21 +66,6 @@ pub struct Ledger<const N: usize> {
 }
 
 impl<const N: usize> Ledger<N> {
-    /// Sort the regions.
-    fn sort(&mut self) {
-        self.regions_mut().sort_unstable_by(|l, r| {
-            if l.addresses == r.addresses {
-                Ordering::Equal
-            } else if l.addresses.is_empty() {
-                Ordering::Greater
-            } else if r.addresses.is_empty() {
-                Ordering::Less
-            } else {
-                l.addresses.start.cmp(&r.addresses.start)
-            }
-        })
-    }
-
     /// Create a new instance.
     pub const fn new(addresses: Line<Address<usize, Page>>) -> Self {
         Self {
@@ -99,13 +81,55 @@ impl<const N: usize> Ledger<N> {
         &self.regions[..self.len]
     }
 
-    /// Get a mutable view of the regions.
-    fn regions_mut(&mut self) -> &mut [Region] {
-        &mut self.regions[..self.len]
+    /// Delete a range of addresses from the regions.
+    pub fn delete(&mut self, addresses: Line<Address<usize, Page>>) -> Result<(), Error> {
+        if addresses.start >= addresses.end {
+            return Err(Error::InvalidRegion);
+        }
+
+        if addresses.start < self.addresses.start || addresses.end > self.addresses.end {
+            return Err(Error::InvalidRegion);
+        }
+
+        for i in 0..self.len {
+            // No contact: skip.
+            let shared = addresses.intersection(self.regions[i].addresses);
+            if shared.is_none() {
+                continue;
+            }
+
+            let region_addresses = self.regions[i].addresses;
+            let value = self.regions[i].token;
+
+            // Region fully covered: remove.
+            if addresses.contains(&region_addresses) {
+                self.remove(i);
+                continue;
+            }
+
+            // Piece fully covered: split the region and return.
+            if region_addresses.contains(&addresses) {
+                self.regions[i].addresses = Line::new(addresses.start, region_addresses.start);
+                return self.insert(Region::new(
+                    Line::new(addresses.end, region_addresses.end),
+                    value,
+                ));
+            }
+
+            // Partially covered: adjust.
+            let shared = shared.unwrap();
+            if shared.start > addresses.start {
+                self.regions[i].addresses = Line::new(addresses.start, shared.start);
+            } else {
+                self.regions[i].addresses = Line::new(shared.end, addresses.end);
+            }
+        }
+
+        Ok(())
     }
 
-    /// Insert a new region into the ledger.
-    pub fn insert(&mut self, region: Region) -> Result<(), Error> {
+    /// Merge a new region to the ledger.
+    pub fn merge(&mut self, region: Region) -> Result<(), Error> {
         if region.addresses.start >= region.addresses.end {
             return Err(Error::InvalidRegion);
         }
@@ -117,17 +141,17 @@ impl<const N: usize> Ledger<N> {
             return Err(Error::InvalidRegion);
         }
 
+        if let Err(e) = self.delete(region.addresses) {
+            return Err(e);
+        }
+
         // Loop over the regions looking for merges.
         let mut iter = self.regions_mut().iter_mut().peekable();
         while let Some(prev) = iter.next() {
-            if prev.addresses.intersection(region.addresses).is_some() {
-                return Err(Error::Overlap);
-            }
+            assert!(prev.addresses.intersection(region.addresses).is_none());
 
             if let Some(next) = iter.peek() {
-                if next.addresses.intersection(region.addresses).is_some() {
-                    return Err(Error::Overlap);
-                }
+                assert!(next.addresses.intersection(region.addresses).is_none());
             }
 
             // Merge previous.
@@ -145,14 +169,7 @@ impl<const N: usize> Ledger<N> {
             }
         }
 
-        if self.len < self.regions.len() {
-            self.regions[self.len] = region;
-            self.len += 1;
-            self.sort();
-            return Ok(());
-        }
-
-        Err(Error::OutOfCapacity)
+        self.insert(region)
     }
 
     /// Find free space from front.
@@ -212,6 +229,51 @@ impl<const N: usize> Ledger<N> {
 
         Err(Error::NoSpace)
     }
+
+    /// Get a mutable view of the regions.
+    fn regions_mut(&mut self) -> &mut [Region] {
+        &mut self.regions[..self.len]
+    }
+
+    /// Insert a region.
+    fn insert(&mut self, region: Region) -> Result<(), Error> {
+        if self.len == self.regions.len() {
+            assert!(self.len <= self.regions.len());
+            return Err(Error::NoCapacity);
+        }
+
+        self.regions[self.len] = region;
+        self.len += 1;
+        self.sort();
+
+        Ok(())
+    }
+
+    /// Remove a region by index.
+    fn remove(&mut self, index: usize) {
+        assert!(self.len > 0);
+        assert!(index < self.len);
+
+        self.regions[index] = self.regions[self.len - 1];
+        self.regions[self.len - 1] = Region::empty(); // clear
+        self.len -= 1;
+        self.sort();
+    }
+
+    /// Sort the regions.
+    fn sort(&mut self) {
+        self.regions_mut().sort_unstable_by(|l, r| {
+            if l.addresses == r.addresses {
+                Ordering::Equal
+            } else if l.addresses.is_empty() {
+                Ordering::Greater
+            } else if r.addresses.is_empty() {
+                Ordering::Less
+            } else {
+                l.addresses.start.cmp(&r.addresses.start)
+            }
+        })
+    }
 }
 
 #[cfg(test)]
@@ -222,13 +284,13 @@ mod tests {
         Line::new(Address::new(0x1000), Address::new(0x10000));
 
     #[test]
-    fn insert() {
+    fn merge() {
         const X: Line<Address<usize, Page>> =
             Line::new(Address::new(0xe000), Address::new(0x10000));
 
         let mut ledger: Ledger<1> = Ledger::new(LIMITS);
         assert_eq!(ledger.len, 0);
-        ledger.insert(Region::new(X, 0)).unwrap();
+        ledger.merge(Region::new(X, 0)).unwrap();
         assert_eq!(ledger.regions(), &[Region::new(X, 0)]);
     }
 
@@ -240,7 +302,7 @@ mod tests {
 
         let mut ledger: Ledger<8> = Ledger::new(LIMITS);
         assert_eq!(ledger.find_free_front(D).unwrap(), A);
-        ledger.insert(Region::new(A, 0)).unwrap();
+        ledger.merge(Region::new(A, 0)).unwrap();
         assert_eq!(ledger.find_free_front(D).unwrap(), B);
     }
 
@@ -253,7 +315,7 @@ mod tests {
 
         let mut ledger: Ledger<8> = Ledger::new(LIMITS);
         assert_eq!(ledger.find_free_back(D).unwrap(), A);
-        ledger.insert(Region::new(A, 0)).unwrap();
+        ledger.merge(Region::new(A, 0)).unwrap();
         assert_eq!(ledger.find_free_back(D).unwrap(), B);
     }
 
@@ -266,9 +328,9 @@ mod tests {
         const Y: Line<Address<usize, Page>> = Line::new(Address::new(0x4000), Address::new(0x6000));
 
         let mut ledger: Ledger<8> = Ledger::new(LIMITS);
-        ledger.insert(Region::new(A, 0)).unwrap();
-        ledger.insert(Region::new(B, 0)).unwrap();
-        ledger.insert(Region::new(X, 0)).unwrap();
+        ledger.merge(Region::new(A, 0)).unwrap();
+        ledger.merge(Region::new(B, 0)).unwrap();
+        ledger.merge(Region::new(X, 0)).unwrap();
 
         assert_eq!(ledger.len, 2);
         assert_eq!(ledger.regions[0], Region::new(Y, 0));
@@ -284,9 +346,9 @@ mod tests {
         const Y: Line<Address<usize, Page>> = Line::new(Address::new(0x7000), Address::new(0x9000));
 
         let mut ledger: Ledger<8> = Ledger::new(LIMITS);
-        ledger.insert(Region::new(A, 0)).unwrap();
-        ledger.insert(Region::new(B, 0)).unwrap();
-        ledger.insert(Region::new(X, 0)).unwrap();
+        ledger.merge(Region::new(A, 0)).unwrap();
+        ledger.merge(Region::new(B, 0)).unwrap();
+        ledger.merge(Region::new(X, 0)).unwrap();
 
         assert_eq!(ledger.len, 2);
         assert_eq!(ledger.regions[0].addresses, A);
